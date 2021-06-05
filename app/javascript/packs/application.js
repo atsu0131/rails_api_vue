c342816a6d00a8a187119b837874268b1d958614
atsushi.isai <atsushi.isai131@gmail.com>
2021-06-05 22:44:48 +0900

    ログイン画面

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,23 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import Vue from 'vue'
+import App from '../app.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+const vuetify = new Vuetify();
+import router from '@/router'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    vuetify,
+    router,
+    render: h => h(App)
+  }).$mount()
+  document.body.appendChild(app.$el)
+  console.log(app)
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -7,9 +7,14 @@
 
 import Vue from 'vue'
 import App from '../app.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+const vuetify = new Vuetify();
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    vuetify,
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
