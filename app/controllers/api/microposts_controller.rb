5737a664b39e0d5ad1712b42dd096403037c5db4
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-06-19 23:34:52 +0900

    CRUD構築

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -1,22 +1,31 @@
 class Api::MicropostsController < ApplicationController
 
-  before_action :authenticate, only: %i[create]
+  before_action :authenticate, only: %i[create update destroy]
   def index
-    microposts = Micropost.includes(:user)
+    microposts = Micropost.includes(:user).order(created_at: :desc)
     render json: microposts, each_serializer: MicropostSerializer
   end
 
   def create
     micropost = current_user.microposts.create!(micropost_params)
-    render json: micropost, serializer: MicropostSerializer, status: :created
+    render json: micropost, serializer: MicropostSerializer
+  end
+  
+  def show
+    micropost = Micropost.find(params[:id])
+    render json: micropost, serializer: MicropostSerializer
   end
 
   def update
-
+    micropost = current_user.microposts.find(params[:id])
+    micropost.update!(micropost_params)
+    render json: micropost, serializer: MicropostSerializer
   end
 
   def destroy
-
+    micropost = current_user.microposts.find(params[:id])
+    micropost.destroy!
+    render json: micropost, serializer: MicropostSerializer
   end
   
   private
